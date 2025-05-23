1a7361c3c00e6777eb81bead218fb4196f32e857
Nathaniel Driggs <nathanieldriggs@gmail.com>
2025-05-22 20:35:21 -0600

    fixing division by zero error

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ sft_fld = soft_fold(2)
 points = np.random.rand(20, 2)
 
 x1 = st.slider(label='x1', min_value=-1.0, max_value=1.0, 
-          step=0.01, value=0.0, key="n_x1")
+          step=0.01, value=0.01, key="n_x1")
 
 y1 = st.slider(label='y1', min_value=-1.0, max_value=1.0, 
-          step=0.01, value=0.0, key="n_y1")
+          step=0.01, value=0.01, key="n_y1")
 
 fig, ax = plt.subplots(figsize=(5,5))
 ax.arrow(0, 0, x1, y1)
