fa4e9d97d471c9b956ae94e154e786f82c5d2389
Nathaniel Driggs <nathanieldriggs@gmail.com>
2025-05-26 16:18:20 -0600

    more fixing crease plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,7 @@ st.markdown("""
 
 def create_softfold_indicator_plot(crease) :
     fig, ax = plt.subplots(figsize=(8,1.5))
-    linspace = np.linspace(-2, 2, 100)
+    linspace = np.linspace(-3, 3, 100)
     ax.plot(linspace, 1 / (1 + np.exp(-linspace * crease)), color='blue')
     ax.set_xlabel("$\mathbf{x} \cdot \mathbf{n} - \mathbf{n} \cdot \mathbf{n}$")
     ax.set_title("How much $\mathbf{x}$ gets folded")
@@ -154,7 +154,7 @@ def create_softfold_indicator_plot(crease) :
 if 'crease' not in st.session_state:
     st.session_state.crease = 1.0
 
-crease = st.slider("Crease", -5.0, 5.0, 1.0, 0.01, key='crease')
+crease = st.slider("Crease", -10.0, 10.0, 1.0, 0.01, key='crease')
 
 fig = create_softfold_indicator_plot(crease)
 st.pyplot(fig, use_container_width=False)
